68b9d7562e1c245f67d89aa9a9b8e7e292734176
awais <devawais.cs@gmail.com>
2023-05-22 01:48:48 +0500

    added authentication on split logs dashboard

--- config/initializers/split.rb
+++ config/initializers/split.rb
@@ -1,6 +1,18 @@
+require 'split/dashboard'
+
 Split.configure do |config|
   config.allow_multiple_experiments = true
   config.redis = Redis.new
   config.persistence = :cookie
   config.persistence_cookie_length = 2592000
+end
+
+Split::Dashboard.use Rack::Auth::Basic do |username, password|
+  ActiveSupport::SecurityUtils.secure_compare(
+    ::Digest::SHA256.hexdigest(username),
+    ::Digest::SHA256.hexdigest(ENV.fetch('SPLIT_USERNAME'))
+  ) & ActiveSupport::SecurityUtils.secure_compare(
+    ::Digest::SHA256.hexdigest(password),
+    ::Digest::SHA256.hexdigest(ENV.fetch('SPLIT_PASSWORD'))
+  )
 end
